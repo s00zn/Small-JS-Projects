218cd209dc999a7259dcc6311a36d2cf05cc0439
s00zn <susan@sheepwns.com>
2020-12-27 13:39:46 +0100

    animate-a-template

---
+++ animated-template/script.js
@@ -0,0 +1,6 @@
+AOS.init({
+    delay: 200,         // values from 0 to 3000, with step 50ms
+    duration: 1500,     // values from 0 to 3000, with step 50ms
+    once: false,        // wether animation should happen only once - while scrolling
+    mirror: false,      // wether elements should animate out while scrolling
+  });
